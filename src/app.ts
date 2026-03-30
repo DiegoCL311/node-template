5c8c707f4cf48c536c3943907713cd3bc91caa14
Diego Cruz <18170307@itculiacan.edu.mx>
2026-03-29 20:09:48 -0700

    Wip: Implementación de Swagger, Seguridad Avanzada, Compresión y Refactor a v1

--- src/app.ts
+++ src/app.ts
@@ -11,25 +11,8 @@ export async function startServer(app: Express) {
 
   await loaders.init({ expressApp: app });
 
-  const server = app.listen(port, () => {
-    logger.info(`Server is running on http://localhost:${port}`);
-  });
-
-  // Graceful shutdown
-  process.on("SIGTERM", () => {
-    logger.info("SIGTERM signal received: closing HTTP server");
-    server.close(() => {
-      logger.info("HTTP server closed");
-      process.exit(0);
-    });
-  });
-
-  process.on("SIGINT", () => {
-    logger.info("SIGINT signal received: closing HTTP server");
-    server.close(() => {
-      logger.info("HTTP server closed");
-      process.exit(0);
-    });
+  app.listen(3000, () => {
+    console.log("Server is running on http://localhost:", port);
   });
 
 }
